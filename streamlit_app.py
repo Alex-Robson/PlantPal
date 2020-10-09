7a37cac33774e54860ddad0922ff65b8ae7ea3bf
Alex <a.robson@berkeley.edu>
2020-10-08 22:26:18 -0700

    Added sample images to app

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,20 +4,14 @@ import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
+import glob
 import PIL
-
 import pandas as pd
 import numpy as np
 from typing import Dict
 
 # define layout
 st.beta_set_page_config(page_title='PlantPal', page_icon=':seedling:', layout='centered', initial_sidebar_state='expanded')
-
-# initialize uploaded file dictionary
-@st.cache(allow_output_mutation=True)
-def get_static_store():
-    """This list is initialized once and can be used to store the files uploaded"""
-    return []
 
 # suppress warning
 st.set_option('deprecation.showfileUploaderEncoding', False)
@@ -36,18 +30,6 @@ class_names = ['Aloe_Vera', 'Asparagus_Fern', 'Baby_Rubber_Plant', 'Boston_Fern'
 # Header
 logo = PIL.Image.open('./data/streamlit/Logo.png')
 st.image(logo, width=700, output_format='PNG') # logo
-
-def predict_image(im):
-    resized_im = im
-    try:
-        np_im = np.array(resized_im)
-        np_im = np_im[None, :]
-        result = model.predict(np_im)
-        index_best = np.argmax(result)
-    except ValueError:
-        st.error('It appears your image isn\'t of the right format, Please input a valid image')
-
-    return class_names[index_best]
 
 def predict_top5(im):
     resized_im = im
@@ -79,8 +61,9 @@ def get_top5_confirmation(top5_plants, top5_probs):
     top5_df.sort_values(by='softmax', ascending=False, inplace=True)
     top5_df.reset_index(inplace=True, drop=True)
 
-    confirmed_indx = display_5_pick_1(top5_df)
-    confirmed_plant = top5_df['name'].iloc[confirmed_indx]
+    confirmed_plant = display_5_pick_1(top5_df)
+    # confirmed_indx = display_5_pick_1(top5_df)
+    # confirmed_plant = top5_df['name'].iloc[confirmed_indx]
 
     print_info(confirmed_plant, im)
 
@@ -89,34 +72,69 @@ def find_key_true(input_dict):
 
 def display_5_pick_1(top5_df):
 
+    # initialize dictionary of which top 5 was selected
     button_dict = {0: False,
                    1: False,
                    2: False,
                    3: False,
                    4: False}
 
-    while all(value == False for value in button_dict.values()) == True:
+    # while (all(value == False for value in button_dict.values()) == True) and :
 
-        st.write('## These are the 5 most likely plants, select the one ')
+    st.write('These are the 5 most likely plants, select the one: \n')
 
-        columns = st.beta_columns(5)
+    columns = st.beta_columns(5)
+    letters = ['A)', 'B)', 'C)', 'D)', 'E)']
+    plant_names = []
 
-        for i, col in enumerate(columns):
+    for i, col in enumerate(columns):
 
-            # get softmax
-            plant_softmax = top5_df.iloc[i]['softmax']
-            softmax_neat = '{:4.4} %'.format(plant_softmax * 100)
+        # get softmax
+        plant_softmax = top5_df.iloc[i]['softmax']
+        softmax_neat = '{:4.4} %'.format(plant_softmax * 100)
 
-            # display token image
-            token_img = PIL.Image.open(f'./data/streamlit/class_imgs/{top5_df.iloc[i].img_file}')
-            token_img = token_img.resize((400,500)) # make image sizes uniform
-            col.image(token_img, width=140, output_format='PNG', caption=softmax_neat)  # logo
+        # get name
+        plant_name = top5_df.iloc[i]['name']
+        plant_names.append(plant_name)
+        col.write(plant_name) # label
 
-            # get name
-            plant_name = top5_df.iloc[i]['name']
-            button_dict[i] = col.button(f'{plant_name}')
+        # display token image
+        token_img = PIL.Image.open(f'./data/streamlit/class_imgs/{top5_df.iloc[i].img_file}')
+        token_img = token_img.resize((400,500)) # make image sizes uniform
+        col.image(token_img, width=140, output_format='PNG', caption=softmax_neat)  # logo
 
-    return find_key_true(button_dict)
+        # # get name
+        # plant_name = top5_df.iloc[i]['name']
+        # button_dict[i] = col.button(f'{plant_name}')
+
+    options4dropdown = plant_names + ['None of the above']
+    search_option = st.selectbox('Select the plant that resembles yours or select none of the above',
+                                 options4dropdown)
+
+    return search_option
+
+        # if st.button('Submit'):
+        #     return search_option
+    # else:
+    #
+    #
+    # submitted = st.button('Submit')
+    # st.write(submitted)
+    # while submitted == False:
+    #     pass
+    # return search_option #find_key_true(button_dict)
+
+    # st.write(all(value == False for value in button_dict.values()))
+    # while 1:
+    #     st.write(all(value == False for value in button_dict.values()))
+    #     st.write(button_dict)
+    #     if all(value == False for value in button_dict.values()) == False:
+    #         return find_key_true(button_dict)
+    # # while all(value == False for value in button_dict.values()) == True:
+    # #     # st.write('while looping')
+    # #     if all(value == False for value in button_dict.values()) == False:
+    # #         return find_key_true(button_dict)
+    # #     pass # wait until this isn't true
 
 def create_model():
 
@@ -136,10 +154,6 @@ def create_model():
         layers.Dense(no_classes, activation='softmax')
     ])
     return model
-
-def load_plant_df():
-    plants_df = pd.read_csv('./data/streamlit/house_plants_16c.csv')
-    return plants_df
 
 def print_info(plant, im):
     # print image
@@ -177,65 +191,71 @@ def print_info(plant, im):
         st.write('     {}'.format(plant_df['dog_tox_desc'].values[0]))
 
 # preload model and information
-plants_df = load_plant_df()
-model = create_model()
-model.load_weights('./models/convmod_1.0_weights.h5')
+plants_df = pd.read_csv('./data/streamlit/house_plants_16c.csv') # plants info
+model = create_model() # model architecture
+model.load_weights('./models/convmod_1.0_weights.h5') # weights
 
-uploaded_files = [] # initialize list of full resolution uploads
-uploaded_files_resized = [] # initialize list of full resolution uploads
-
-####### Sidebar #######
-# Pick from list
+# SIDEBAR
+# Choose use mode
 st.sidebar.write('## Search Method:')
-search_option = st.sidebar.selectbox(
-    '',
-     ['upload an image', 'from a list'])
+search_option = st.sidebar.selectbox('', ['upload an image', 'from a list'])
 
+# Place sample images
 st.sidebar.write('## Sample Images:')
 
-# img = PIL.Image.open('./data/streamlit/download.jpeg')
-# st.sidebar.image(img)
+for file in glob.glob('./data/streamlit/sample_imgs/*'):
+    sample_img = PIL.Image.open(file)
+    sample_img = sample_img.resize((150,200))
+    st.sidebar.image(sample_img)
 
-#######################
-
+# SELECTING FROM LIST
 if search_option == 'from a list':
-    option = st.selectbox(
+    plant = st.selectbox(
         'Select a plant by name:',
          plants_df['name'])
 
-    'You selected: ', option
+    # select local image
+    local_img_file = plants_df[plants_df['name'] == plant].img_file.values[0]
+    token_img = PIL.Image.open(f'./data/streamlit/class_imgs/{local_img_file}')
+    token_img = token_img.resize((700, 700))  # make image sizes uniform
 
+    print_info(plant, token_img)
+
+# USING AN UPLOADED IMAGE
 elif search_option == 'upload an image':
 
-    st.write('upload an image of a house plant:')
+    # read in the file
     uploaded_file = st.file_uploader("", type=["jpg", "jpeg", "png"])
 
+    # preprocess and append array
     if uploaded_file:
         # Process you file here
         im = PIL.Image.open(uploaded_file)
+        np_im = np.array(im) # full resolution np array
+        im240 = im.resize((240, 240))
+        np_im240 = np.array(im240) # resize for model
 
-        if not im in uploaded_files:
-            im = PIL.Image.open(uploaded_file) # TIDY THIS
-            # im700 = im.resize((700, 700))
-            np_im700 = np.array(im)
-            uploaded_files.append(np_im700)
-
-            # im700 = im.resize((700, 700))
-            # np_im700 = np.array(im700)
-            # uploaded_files.append(np_im700)
-
-            im = PIL.Image.open(uploaded_file)
-            im240 = im.resize((240, 240))
-            np_im240 = np.array(im240)
-            uploaded_files_resized.append(np_im240)
     else:
-        st.info("Please upload at least one image of a house plant")
+        st.info("Please upload at least one image of a house plant or select \"from list\" in the sidebar")
 
     if uploaded_file:
-        # if len(uploaded_files) == 1:
-        #
-        top5_plants, top5_probs = predict_top5(uploaded_files_resized[0])
-        get_top5_confirmation(top5_plants, top5_probs)
 
-        # name = predict_image(uploaded_files_resized[0])
-        # print_info(name.replace('_', ' '), uploaded_files[0])
+        top5_plants, top5_probs = predict_top5(np_im240)
+
+        # If unlikely to be a plant
+        # blah
+
+        # If very confident
+        if max(top5_probs) > 0.95:
+            plant = top5_plants(np.argmax(top5_probs))
+            print_info(plant.replace('_', ' '), im)
+
+        # If partially confident
+        elif max(top5_probs) > 0.5:
+            get_top5_confirmation(top5_plants, top5_probs)
+
+        # If very uncertain
+        else:
+            st.write('## Unfortunately there is a high uncertainty with your image - consider taking another')
+
+        # get_top5_confirmation(top5_plants, top5_probs)
